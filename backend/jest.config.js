c3e747990e8f4ddd517c48f45e617c71b827d579
Madhav <madhav0570@gmail.com>
2025-11-10 15:41:58 +0530

    confix fix

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -12,10 +12,10 @@ module.exports = {
     setupFiles: ['./src/tests/setup-env.js'],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
     }
   }
 };
